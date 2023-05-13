8c116ad93c4404e28678cf91541b351c23b815db
Andre Coimbra <andre_coimbra1994@hotmail.com>
2023-05-12 21:46:25 -0300

    atualizar usuarios

--- backend/src/main/java/com/projetointegrador/backend/services/UserService.java
+++ backend/src/main/java/com/projetointegrador/backend/services/UserService.java
@@ -11,7 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.projetointegrador.backend.dto.UserDTO;
 import com.projetointegrador.backend.entities.User;
 import com.projetointegrador.backend.repositories.UserRepository;
-import com.projetointegrador.backend.services.exceptions.EntityNotFoundException;
+import com.projetointegrador.backend.services.exceptions.ResourceNotFoundException;
+import javax.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -28,7 +30,7 @@ public class UserService {
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		User entity = obj.orElseThrow(() -> new EntityNotFoundException("usuario nao localizado"));
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("usuario nao localizado"));
 		return new UserDTO(entity) ;
 	}
 	
@@ -44,6 +46,25 @@ public class UserService {
 		entity.setSenha(dto.getSenha());
 		entity = repository.save(entity);
 		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO update(Long Id, UserDTO dto) {
+		try {
+		User entity = repository.getReferenceById(Id);
+		entity.setName(dto.getName());
+		entity.setDataNascimento(dto.getDataNascimento());
+		entity.setAnoFormacao(dto.getAnoFormacao());
+		entity.setCidade(dto.getCidade());
+		entity.setUf(dto.getUf());
+		entity.setEmail(dto.getEmail());
+		entity.setSenha(dto.getSenha());
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Usuario" + dto.getName() + "nao encontrado");
+		}
 	}
 
 }
